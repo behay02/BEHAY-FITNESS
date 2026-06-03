45b0807282ce3e92f1d2fa5c59d54e0a8a3e61f5
behay02 <behruzhaydarov06@gmail.com>
2026-06-03 21:47:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -195,3 +195,11 @@ function renderTrainers() {
         }
     });
 }
+function toggleTheme() {
+    document.body.classList.toggle('light-theme');
+    localStorage.setItem('theme', document.body.classList.contains('light-theme') ? 'light' : 'dark');
+}
+
+if (localStorage.getItem('theme') === 'light') {
+    document.body.classList.add('light-theme');
+}
